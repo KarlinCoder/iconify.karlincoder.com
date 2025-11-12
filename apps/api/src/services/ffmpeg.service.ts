c99131c7ddc7aaa35aca0dd3c1f52ace63449854
KarlinCoder <giancarlo.frontend@gmail.com>
2025-11-12 18:24:04 -0500

    added ffmpeg-static

--- apps/api/src/services/ffmpeg.service.ts
+++ apps/api/src/services/ffmpeg.service.ts
@@ -1,45 +1,49 @@
 import { spawn } from "child_process";
 import path from "path";
-import { getFfmpegPath } from "../utils/get-ffmpeg-path";
-import { checkTmpPaths } from "../utils/check-tmp-paths";
 import { BaseConfig } from "../config/base.config";
+import ffmpegPathStatic from "ffmpeg-static";
 
-const ffmpegPath = getFfmpegPath();
-console.log(ffmpegPath);
+if (!ffmpegPathStatic) {
+  throw new Error("Ffmpeg path static");
+}
+
+const ffmpegPath: string = ffmpegPathStatic;
 
 export class FfmpegService {
   static async generateIcon(
     imgUrl: string,
     format: string,
     resolution: string
-  ): Promise<string> {
-    checkTmpPaths();
-
+  ) {
     const outputFile = path.resolve(
       path.join(BaseConfig.tmpFiles.outputFiles, `${Date.now()}_icon.${format}`)
     );
 
-    return new Promise((resolve, reject) => {
-      const imageProcess = spawn(ffmpegPath, [
-        "-i",
-        imgUrl,
-        "-vf",
-        `scale=${parseInt(resolution)}:-1`,
-        outputFile,
-      ]);
+    if (ffmpegPath) {
+      return new Promise<string>((resolve, reject) => {
+        const imageProcess = spawn(ffmpegPath, [
+          "-i",
+          imgUrl,
+          "-vf",
+          `scale=${parseInt(resolution)}:-1`,
+          outputFile,
+        ]);
 
-      imageProcess.stderr.on("error", (data) => {
-        reject(new Error("Hubo un error"));
-      });
+        imageProcess.stderr.on("error", () => {
+          reject(new Error("Hubo un error"));
+        });
 
-      imageProcess.on("close", (code) => {
-        if (code !== 0) {
-          reject(new Error(`Proceso de conversión fallido con código ${code}`));
-        } else {
-          console.log(`Icono generado en: ${outputFile}`);
-          resolve(outputFile);
-        }
+        imageProcess.on("close", (code) => {
+          if (code !== 0) {
+            reject(
+              new Error(`Proceso de conversión fallido con código ${code}`)
+            );
+          } else {
+            console.log(`Icono generado en: ${outputFile}`);
+            resolve(outputFile);
+          }
+        });
       });
-    });
+    }
   }
 }
