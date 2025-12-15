e0e3f0bfe3e59fe4b2432897fa2b3102d8c7fc4c
Giancarlo Dennis <giancarlo.frontend@gmail.com>
2025-12-15 08:34:11 -0500

    Update server.ts

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -2,6 +2,6 @@ import { app } from "./app";
 import { BaseConfig } from "./config/base.config";
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`✅ API running on port ${PORT}`);
 });
